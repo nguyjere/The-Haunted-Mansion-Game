6927d6f7591af74418ae7f43333ddcea7abc9ba9
Jeremy Nguyen <nguyjere@oregonstate.edu>
2019-01-21 23:22:50 -0800

    Wrote out the main menu with placeholders

--- haunted_mansion_game/game.py
+++ haunted_mansion_game/game.py
@@ -3,14 +3,30 @@ Contains the main loop of the game and core functions
 """
 
 
+def main_main():
+    print "Title"
+    print "Main Menu"
+    print "[1] Start New Game"
+    print "[2] Load Game"
+    print "[3] Exit"
+    return raw_input(">>")
+
+
 def play():
     # game loop here
-    pass
+    while True:
+        print "Displays current room description"
+        print "Displays features and objects in the room"
+        user_input = raw_input(">>")
+        print "Parse and execute action with user input: {}".format(user_input)
+        if user_input == "end game":
+            print "Game Over"
+            break
 
 
-def load_game():
+def load_game(saved_game):
+    print "Loading {0}...".format(saved_game)
     # loads player, room, and item data
-    pass
 
 
 def save_game():
@@ -18,6 +34,19 @@ def save_game():
     pass
 
 
+def select_saved_games():
+    print "Here's a list of saved games. Pick one"
+    # display list of saved game files
+    return raw_input(">>")
+
+
 if __name__ == "__main__":
-    # display main menu to start new game, or load game
-    play()
+    menu_input = main_main()
+    if menu_input == '1':
+        play()
+    elif menu_input == '2':
+        selected_game_save = select_saved_games()
+        load_game(selected_game_save)
+        play()
+    elif menu_input == '3':
+        exit()
