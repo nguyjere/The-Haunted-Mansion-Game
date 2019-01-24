286060fdbb29e61d84bee82219ff08102413179e
nowickis <nowickis@oregonstate.edu>
2019-01-23 21:01:52 -0600

    move vocab to separate file; make interpretRoom return empty dict when no command found; interpretLook function

--- command_line_parser/textParser.py
+++ command_line_parser/textParser.py
@@ -1,17 +1,19 @@
+import vocabulary
 class TextParser:
 
-    rooms = ["attic", "bar", "basement", "diningroom", "garage", "garden", "guestroom", "kitchen", "library", "livingroom", "lounge", "masterbedroom", "secretroom", "stewardsroom"] 
 
-    verbs = ["look", "go", "take", "help", "inventory", "savegame", "loadgame", "lift", "drop", "push", "pull", "consume", "open", "close", "turnon", "turnoff", "hit"]
-
-    directions = ["north", "south", "east", "west"]
-
-    def _init_(self, gameId):
+    def __init__(self, gameId):
         self.gameId = gameId
+        self.verbs = vocabulary.verbs
+        self.rooms = vocabulary.rooms
+        self.directions = vocabulary.directions
+        self.features = vocabulary.features
+        self.prepositions = vocabulary.prepositions
 
     def promptUser(self, prompt):
         command = raw_input(prompt)
         return command
+
     #parses commands
     def parseCommand(self, command):
         words = command.split()
@@ -19,19 +21,31 @@ class TextParser:
 
     # parameters 
     # words: a command
-    def interpretBasic(self, command):
+    # interpretRoom, among other interpret commands can be used in a loop to figure out the user command
+    # if interpretRoom returns a non-empty dictionary, then we know the user wants to go to a new room
+    def interpretRoom(self, command):
         parsedWords = self.parseCommand(command)
         verb = self.findWord(parsedWords, "verb")
         room = self.findWord(parsedWords, "room")
         direction = self.findWord(parsedWords, "direction")
-        
         userCommandDict = {"verb":verb, "room": room, "direction":direction}
         valid = self.errorCheckRoomCommand(userCommandDict)
-        
-        if valid == True:
-            return userCommandDict
-        else:
-            return "Invalid Command"
+        if valid != True:
+            userCommandDict = {"verb": '', "room": '', "direction": ''}
+        return userCommandDict
+
+    # if just look is returned, then repeat description of room
+    # if look at feature is returned, then describe feature
+    def interpretLook(self, command):
+        parsedWords = self.parseCommand(command)
+        verb = self.findWord(parsedWords, "verb")
+        feature = self.findWord(parsedWords, "feature")
+        preposition = self.findWord(parsedWords, "preposition")
+        userCommandDict = {"verb": verb, "feature": feature, "preposition": preposition}
+        valid = self.errorCheckLookCommand(userCommandDict)
+        if valid != True:
+            userCommandDict = {"verb": '', "feature": '', "preposition": ''}
+        return userCommandDict
             
     
     def findWord(self, words, type):
@@ -42,6 +56,10 @@ class TextParser:
             listToSearch = self.rooms
         if type == "direction":
             listToSearch = self.directions
+        if type == "feature":
+            listToSearch = self.features
+        if type == "preposition":
+            listToSearch = self.prepositions
         for word in words:
             for c in listToSearch:
                 if word == c:
@@ -54,6 +72,16 @@ class TextParser:
         if userCommandDict["verb"] == "go" and (userCommandDict["room"] != "" or userCommandDict["direction"] != ""):
             return True
         elif userCommandDict["verb"] == "" and (userCommandDict["room"] != "" or userCommandDict["direction"] != ""):
+            return True
+        else:
+            return False
+
+    def errorCheckLookCommand(self, userCommandDict):
+        if userCommandDict["verb"] == "look" and userCommandDict["feature"] == "" and \
+                userCommandDict["preposition"] == "":
+            return True
+        elif userCommandDict["verb"] == "look" and userCommandDict["feature"] != "" and \
+                userCommandDict["preposition"] == "at":
             return True
         else:
             return False
---
+++ command_line_parser/vocabulary.py
@@ -0,0 +1,11 @@
+rooms = ["attic", "bar", "basement", "diningroom", "garage", "garden", "guestroom", "kitchen", "library", "livingroom",
+         "lounge", "masterbedroom", "secretroom", "stewardsroom"]
+
+verbs = ["look", "go", "take", "help", "inventory", "savegame", "loadgame", "lift", "drop", "push", "pull", "consume",
+         "open", "close", "turnon", "turnoff", "hit"]
+
+directions = ["north", "south", "east", "west"]
+
+features = ["feature1"]
+
+prepositions =["at"]
