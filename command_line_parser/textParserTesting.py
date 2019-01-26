d0cef1aa54261c6d8b7aeb2bd473cafd82bbf9c9
nowickis <nowickis@oregonstate.edu>
2019-01-26 17:25:26 -0600

    1. make interpret functions accept room represented as dictionary so that returned dict will only contain entry if room or feature is available in that room 2. changes to room files to simplify parsing. no camel case, change 'bar cabinet' to 'cabinet', change 'washerMachine' to 'washingmachine' 3. add preparsing for features, to handle features with spaces, i.e. pool table

--- command_line_parser/textParserTesting.py
+++ command_line_parser/textParserTesting.py
@@ -1,14 +1,23 @@
 from textParser import TextParser
 
-textParser = TextParser(123)
+textParser = TextParser()
+testRoomDict = {
+  "roomName": "bar",
+  "connectedTo": ["livingroom"],
+  "displayRoomName": "Bar",
+  "features": ["stools","pooltable", "barcounter", "cabinet", "frenchdoor"],
+  "objects": ["winebottle"],
+  "visited": "false"
+}
+
 while True:
     command = textParser.promptUser('Where to? ')
-    parsedCommand = textParser.interpretRoom(command)
+    parsedCommand = textParser.interpretRoom(command, testRoomDict)
     print parsedCommand
 
-    #command = textParser.promptUser('Look where? ')
-    #parsedCommand = textParser.interpretLook(command)
-    #print parsedCommand
+    command = textParser.promptUser('Look where? ')
+    parsedCommand = textParser.interpretLook(command, testRoomDict)
+    print parsedCommand
 
     ##command = textParser.promptUser('Where to? ')
     ##parsedCommand = textParser.preParseRoomCommand(command)
