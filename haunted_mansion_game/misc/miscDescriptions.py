698e62dd03f192c28eecd9d3c9edb2f41952ee9d
nowickis <nowickis@oregonstate.edu>
2019-03-09 07:57:22 -0600

    add intro text; remove unneeded room entries

---
+++ haunted_mansion_game/misc/miscDescriptions.py
@@ -0,0 +1,17 @@
+introduction = "\nWhat is going on? This is so strange. The last thing you remember, you were " \
+               "in your bed at home. Not anymore.\n"\
+                "You're in some sort of dark mansion. Your head " \
+               "hurts and with each step you take you're feeling worse.\n" \
+               "You need to get out of here. Soon.\n"\
+                "But how???\n\n"\
+                "Move from room to room using: "\
+               "'go [room]', '[room]', 'go [direction]', '[direction]'\n"\
+                "Examine rooms using: 'look'\n"\
+                "Examine things in rooms using: 'look at [thing]'\n"\
+                "Other things you can do: \n"\
+               "'take [thing]'\n'help'\n'inventory'\n'savegame'\n'loadgame'\n'endgame'\n"\
+               "'lift [thing]'\n'drop [thing]'\n'push [thing]'\n'pull [thing]'\n'consume [thing]'\n" \
+               "'open [thing]'\n'close [thing]'\n'turn on [thing]'\n'turn off [thing]'\n'hit [thing]'\n" \
+                "'drive [thing]'\n"\
+                "Keep in mind that commands have synonyms too...\n"\
+                "Good luck.\n\n\n"
